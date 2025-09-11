5c686658b6a115ba4fd9b4ebab53557322f813d6
SantanuAdhikary <santanuadhikary13@gmail.com>
2025-09-11 13:35:38 +0530

    fetch in js

---
+++ JAVASCRIPT/10_fetch/fetch.js
@@ -0,0 +1,25 @@
+
+let fetchedData =  fetch("https://fakestoreapi.com/products")
+
+
+console.log(fetchedData)
+
+
+fetchedData.then((data)=>{
+
+     console.log(data)  // response 
+
+     let jsondata = data.json()
+     console.log(jsondata)   // promise 
+
+
+     jsondata.then((data)=>{
+            console.log(data)  
+     })
+     .catch((err)=>{
+        console.log(err)
+     })
+})
+.catch((err)=>{
+    console.log(err)
+})
